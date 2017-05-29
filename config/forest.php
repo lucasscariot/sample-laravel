8923846ee561a06afb3bae54507df58748f2fca1
Lucas Scariot <lucas@forestadmin.com>
2017-05-29 11:16:33 +0200

    [+] Structure

---
+++ config/forest.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'url' => 'http://localhost:3001',
+    'secret_key' => 'd504e4f5b508e2627a9611724da748294e202287c47bfc3a522f91f7ae3ffe2a',
+    'auth_key' => 'nHJum5dd59bNtBawmu70LkevP43whklk',
+    'models_path' => 'app'
+];
